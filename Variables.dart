56e537a10e10b07883d0275612e2af490f44b3da
Luis Emilio Gomez Gonzalez <gomezgonzalezluisemilio188@gmail.com>
2025-11-16 22:04:21 -0600

    Segundo comit se vio lo que son la variables del lenguaje dart

---
+++ Variables.dart
@@ -0,0 +1,26 @@
+/** Tenemos que un archivo dart debe tener su funcion main que se ve de la siguiente manera:
+ * esta empieza copn void que significa vacio, siguiendo con el main con parentesis juntos y llaves que es donde ira el codigo a ejecutar
+ */
+void main() {
+  // Aqui va el codigo
+  // Tenemos que la variable es una caja en la cual se guarda informacion
+  // Para trabajar en dart tenemos que iniciar definiendo la palabra var
+  var edad = 10;
+  var temperatura = 18.5;
+  var nombre = "David";
+  var estado = true;
+
+  // Tambien funcionan asignandole un tipo de variable
+  int edad2 = 10;
+  double temperatura2 = 18.5;
+  String nombre2 = "David";
+  bool estado2 = true;
+
+  //Tambien se puede utilizar si haber iniciado la variable
+  String cualquiercosa;
+
+  cualquiercosa = "hola";
+  //Para debugear codigo usamos print que imprime en consola
+  //Tambien podemos inicalizarlo busacndo el aarchivo y poniedno dart al inicio
+  print(edad);
+}
